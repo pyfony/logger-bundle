dcc6213125f6e23852419ed8034bfcd1b1916ff2
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-15 06:14:35 +0100

    Option to set default log level added

--- src/loggerbundle/LoggerFactory.py
+++ src/loggerbundle/LoggerFactory.py
@@ -5,13 +5,15 @@ class LoggerFactory:
 
     def __init__(
         self,
-        handlers: List[Handler]
+        defaultLogLevel: int,
+        handlers: List[Handler],
     ):
+        self.__defaultLogLevel = defaultLogLevel
         self.__handlers = handlers
 
-    def create(self, loggerName: str, logLevel: int):
+    def create(self, loggerName: str, logLevel: int = None):
         logger = getLogger(loggerName)
-        logger.setLevel(logLevel)
+        logger.setLevel(logLevel if logLevel is not None else self.__defaultLogLevel)
 
         logger.handlers = self.__handlers
         logger.propagate = False
