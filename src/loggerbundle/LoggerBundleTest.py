451c2978929e5dcfac58727f9fa8961591683f18
Jiri Koutny <jirkakoutny@gmail.com>
2021-01-04 16:46:09 +0100

    Updated for pyfony 0.7.*

--- src/loggerbundle/LoggerBundleTest.py
+++ src/loggerbundle/LoggerBundleTest.py
@@ -1,30 +1,11 @@
 import unittest
 from injecta.testing.servicesTester import testServices
-from injecta.config.YamlConfigReader import YamlConfigReader
-from injecta.package.pathResolver import resolvePath
-from typing import List
-from pyfony.kernel.BaseKernel import BaseKernel
-from pyfonybundles.Bundle import Bundle
-from loggerbundle.LoggerBundle import LoggerBundle
+from pyfonycore.bootstrap import bootstrappedContainer
 
 class LoggerBundleTest(unittest.TestCase):
 
     def test_init(self):
-        class Kernel(BaseKernel):
-
-            def _registerBundles(self) -> List[Bundle]:
-                return [
-                    LoggerBundle()
-                ]
-
-        kernel = Kernel(
-            'test',
-            resolvePath('loggerbundle') + '/_config',
-            YamlConfigReader()
-        )
-
-        container = kernel.initContainer()
-
+        container = bootstrappedContainer.init('test')
         testServices(container)
 
 if __name__ == '__main__':
